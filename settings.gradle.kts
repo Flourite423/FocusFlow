08eb8583298024236d7ba65b6f039df01cc8b5f0
Flourite423 <1006503427@qq.com>
2026-06-22 18:02:32 +0800

    fix(build): add dependency resolution repositories in settings
    Add dependencyResolutionManagement with google() and mavenCentral()
    to resolve 'no repositories are defined' error in CI.

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -13,6 +13,14 @@ pluginManagement {
     }
 }
 
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
 rootProject.name = "FocusFlow"
 
 include(":app")
