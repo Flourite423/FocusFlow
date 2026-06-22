42caa05df24d748f1201240c4dd98678e6055f38
Flourite423 <1006503427@qq.com>
2026-06-22 17:57:24 +0800

    fix(build): resolve Hilt plugin and Gradle version issues
    - settings.gradle.kts: add resolutionStrategy to map Hilt plugin ID
      to hilt-android-gradle-plugin artifact
    - gradle-wrapper.properties: pin to Gradle 8.11.1 (was 9.6.0)

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -4,6 +4,13 @@ pluginManagement {
         mavenCentral()
         gradlePluginPortal()
     }
+    resolutionStrategy {
+        eachPlugin {
+            if (requested.id.id == "com.google.dagger.hilt") {
+                useModule("com.google.dagger:hilt-android-gradle-plugin:${requested.version}")
+            }
+        }
+    }
 }
 
 rootProject.name = "FocusFlow"
